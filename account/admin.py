9e216444c2f99228709344a3eff21a4509fbd6f5
Ivan <jonnykold@mail.ru>
2022-09-26 23:58:15 +0200

    Register Album and User admin

--- account/admin.py
+++ account/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from account.models import Photo
+from account.models import Album, Photo, User
 
 
 @admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Album)
+class PhotoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
 class PhotoAdmin(admin.ModelAdmin):
     pass
